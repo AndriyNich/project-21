13a2dd54d92867fe7fad051c623e5845616eae86
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2022-11-18 21:10:03 +0000

    Deploying to gh-pages from @ AndriyNich/project-21@fd01ed4730927efa004d9a1b7aabbf21b3adc01c 🚀

---
+++ accessories.d0e0b097.js
@@ -0,0 +1,2 @@
+!function(){var e,t,a;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),a=document.body,e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.getAttribute("aria-expanded",!n),t.classList.toggle("is-open"),a.classList.toggle("modal-open")}))}();
+//# sourceMappingURL=accessories.d0e0b097.js.map
